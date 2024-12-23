d0e7e07a1c6706c7f336965eddb4b31672a0aeff
Vaidotak <pehtrauhk@gmail.com>
2024-12-23 09:36:48 +0200

    Patvarkyta samba

--- services.nix
+++ services.nix
@@ -13,50 +13,55 @@
   #  services.searx.settings.server.port = 8080;
   #  services.searx.settings.search.formats = [ "html" "json" "rss" ];
 
-  services.openssh.enable = true;
+  #  services.openssh.enable = true;
 
   services.espanso = {
     enable = true;
     package = pkgs.espanso;
   };
 
-  services.prometheus = {
-    enable = true;
-    globalConfig.scrape_interval = "10s"; # "1m"
-    scrapeConfigs = [
-      {
-        job_name = "node";
-        static_configs = [{
-          targets = [ "localhost:${toString config.services.prometheus.exporters.node.port}" ];
-        }];
-      }
-    ];
-  };
+  # services.prometheus = {
+  #   enable = true;
+  #   globalConfig.scrape_interval = "10s"; # "1m"
+  #   scrapeConfigs = [
+  #     {
+  #       job_name = "node";
+  #       static_configs = [{
+  #         targets = [ "localhost:${toString config.services.prometheus.exporters.node.port}" ];
+  #       }];
+  #     }
+  #   ];
+  # };
 
-  services.grafana = {
-    enable = true;
-    settings = {
-      server = {
-        http_addr = "127.0.0.1";
-        http_port = 3000;
-        enforce_domain = false;
-        enable_gzip = true;
-      };
-    };
-  };
+  # services.grafana = {
+  #   enable = true;
+  #   settings = {
+  #     server = {
+  #       http_addr = "127.0.0.1";
+  #       http_port = 3000;
+  #       enforce_domain = false;
+  #       enable_gzip = true;
+  #     };
+  #   };
+  # };
 
-  services.prometheus.exporters.node = {
-    enable = true;
-    port = 9100;
-  };
+  # services.prometheus.exporters.node = {
+  #   enable = true;
+  #   port = 9100;
+  # };
 
- # Importuojame kelius iš samba-paths.nix failo
   services.samba = {
     enable = true;
     openFirewall = true;
     settings = {
       global = {
-        "workgroup" = "WORKGROUP";
+        "server min protocol" = "SMB2";
+        "server max protocol" = "SMB3";
+        "encrypt passwords" = "yes";
+        #"smb encrypt" = "required";
+        #"workgroup" = "WORKGROUP";
+        "log level" = "2";
+        "log file" = "/home/vaidotak/samba/log.%m";
         "server string" = "smbnix";
         "netbios name" = "smbnix";
         "security" = "user";
@@ -72,8 +77,9 @@
       in {
         "path" = sambaPaths.sambaPaths.moviesPath;
         "browseable" = "yes";
-        "read only" = "no";
+        "read only" = "yes";
         "guest ok" = "yes";
+        "hosts allow" = "192.168.1.206 192.168.1.242"; # Android TV
         "create mask" = "0644";
         "directory mask" = "0755";
         "force user" = "vaidotak";
@@ -85,8 +91,9 @@
       in {
         "path" = sambaPaths.sambaPaths.videoPath;
         "browseable" = "yes";
-        "read only" = "no";
+        "read only" = "yes";
         "guest ok" = "yes";
+        "hosts allow" = "192.168.1.206 192.168.1.242"; # Android TV
         "create mask" = "0644";
         "directory mask" = "0755";
         "force user" = "vaidotak";
