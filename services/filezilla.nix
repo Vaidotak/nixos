f0dfd0217a4a05c42202b4ca996b22185f643e91
Vaidotak <pehtrauhk@gmail.com>
2025-02-05 01:19:50 +0200

    Automatinis atnaujinimas: 2025-02-05 01:19:50

--- services/filezilla.nix
+++ services/filezilla.nix
@@ -4,22 +4,24 @@ let
   secrets = import /home/vaidotak/.config/syncthing/secret.nix;
 in {
   programs.filezilla = {
-    enable = true;
-    settings = {
-      "Servers" = {
-        "Server" = [
+  enable = true;
+  settings = {
+    FileZilla3 = {
+      Servers = {
+        Server = [
           {
-            "Host" = secrets.filezilla.host;
-            "Port" = "22";
-            "Protocol" = "0";
-            "Type" = "0";
-            "User" = secrets.filezilla.user;
-            "Pass" = secrets.filezilla.pass; 
-            "Name" = "Knygų serveris";
-            "RemoteDir" = secrets.filezilla.remoteDir;
+            Host = secrets.filezilla.host;
+            Port = "22";
+            Protocol = "0";
+            Type = "0";
+            User = secrets.filezilla.user;
+            Pass = secrets.filezilla.pass;
+            Name = "Knygų serveris";
+            RemoteDir = secrets.filezilla.remoteDir;
           }
         ];
       };
     };
   };
+};
 }
