cefe737b52149a9495d7160886b1651cb34583b5
Vaidotak <pehtrauhk@gmail.com>
2025-03-02 05:32:38 +0200

    atnaujintos darbalaukių aplinkos ir šriftai

--- system/modules/desktop.nix
+++ system/modules/desktop.nix
@@ -1,9 +1,9 @@
 { config, pkgs, ... }:
-
 {
   services = {
     xserver = {
       enable = true;
+      
       displayManager = {
         lightdm = {
           enable = true;
@@ -13,22 +13,30 @@
           };
         };
         sessionCommands = ''
-        xrandr --output HDMI-1 --off
-      '';
+          xrandr --output HDMI-1 --off
+        '';
       };
-      windowManager.i3.enable = false;
-      windowManager.dwm.enable = false;
+      
+      # Langų valdikliai
+      windowManager = {
+        i3.enable = false;
+        dwm.enable = false;
+      };
+      
+      # Darbalaukio aplinkos (XFCE)
       desktopManager = {
-        #plasma5.enable = true;
-        xterm.enable = false;
         xfce.enable = true;
+        xterm.enable = false;
       };
-
+      
+      # Klaviatūros nustatymai
       xkb = {
         layout = "lt";
         variant = "";
       };
     };
+    
+    # Teisingi keliai šioms opcijoms
     displayManager = {
       defaultSession = "xfce";
       autoLogin = {
@@ -36,30 +44,47 @@
         user = "vaidotak";
       };
     };
+    
+    # Teisingas kelias Plasma 6
     desktopManager.plasma6.enable = true;
   };
+  
+  # Konsolės nustatymai
   console.keyMap = "lt.baltic";
+  
+  # Kompozitoriai
   programs.hyprland.enable = true;
   
+  # Šriftų nustatymai
   fonts = {
-  enableDefaultPackages = true;
-  # fonts = with pkgs; [
-  #   tex-gyre-fonts  # TeX Gyre šriftai
-  #   fira-code        # Fira Sans ir kiti šriftai
-  #   font-awesome     # Ikonoms
-  #   nerdfonts       # Jeigu naudosite Nerd Fonts
-  # ];
-  fontconfig = {
-    enable = true;
-    defaultFonts = {
-      sansSerif = [ "Noto Sans Regular" ];
-      monospace = [ "Monospace Regular" ];
-    };
-    hinting = {
+    enableDefaultPackages = true;
+    
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-emoji
+      liberation_ttf
+      fira-code
+      font-awesome
+      # Naujas būdas nurodyti Nerd Fonts
+      nerd-fonts.fira-code
+      nerd-fonts.droid-sans-mono
+    ];
+    
+    fontconfig = {
       enable = true;
-      style = "full";  # Pilnas glotninimas
+      defaultFonts = {
+        sansSerif = [ "Noto Sans" ];
+        serif = [ "Noto Serif" ];
+        monospace = [ "Fira Code" ];
+        emoji = [ "Noto Color Emoji" ];
+      };
+      hinting = {
+        enable = true;
+        style = "full";
+      };
+      subpixel.rgba = "rgb";
+      antialias = true;
     };
-    antialias = true;  # Įjungia anti-aliasing
   };
-};
 }
